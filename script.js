5819db473f2eacb03c62011cfdee886dfad8a0cf
Krumbledore <45559583+Krumbledore@users.noreply.github.com>
2024-05-09 15:51:34 -0800

    add math functions and function to call math function based on operator

--- script.js
+++ script.js
@@ -3,12 +3,37 @@ function operator(num1, op, num2){
         case '+':
             add(num1, num2);
             break;
+        
         case '-':
-            console.log('subtraction')
+            subtract(num1, num2);
+            break;
+
+        case '*':
+            multiply(num1, num2);
+            break;
+            
+        case '/':
+            divide(num1, num2);
+            break;
     }
 }
 
 function add(num1, num2){
-    sum = num1 + num2;
+    const sum = num1 + num2;
     console.log(sum)
+}
+
+function subtract(num1, num2){
+    const result = num1 - num2;
+    console.log(result)
+}
+
+function multiply(){
+    const product = num1 * num2;
+    console.log(product);
+}
+
+function divide(num1, num2){
+    const result = num1 / num2;
+    console.log(result);
 }
